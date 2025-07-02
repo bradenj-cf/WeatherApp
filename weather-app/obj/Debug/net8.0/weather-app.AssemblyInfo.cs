55abd43f6d73a3b8aa3ca5116e160dda1e278850
Braden Johnson <braden.johnson@cf.com>
2025-07-02 16:42:50 -0400

    fix

--- weather-app/obj/Debug/net8.0/weather-app.AssemblyInfo.cs
+++ weather-app/obj/Debug/net8.0/weather-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("weather-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8559d1bd92ffeb2bc323076b508c13340bda3006")]
 [assembly: System.Reflection.AssemblyProductAttribute("weather-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("weather-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
